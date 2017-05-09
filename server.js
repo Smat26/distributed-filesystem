7c424cef9b7c96af6ef935bc7d48fd83ec9f3fc9
Ahsan <ahsan@172-15-198-185.lightspeed.bcvloh.sbcglobal.net>
2017-05-09 18:20:50 +0500

    Changed name

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var figlet = require('figlet');
 const main = () => {
 
   startWatch();
-  figlet('Gormint FS', function(err, data) {
+  figlet('Distribute', function(err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
